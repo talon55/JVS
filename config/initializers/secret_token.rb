81e0ad744c9b2702f68a42dbc4d252ecc234bb00
Eric Madar <emsgeek@gmail.com>
2011-12-24 15:43:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jvs::Application.config.secret_token = '02f2efb6e13575905c55e2e54d824ee06297b53a6b0ea05c9bed4484e38bb5c23bbbea9c26c066228dc6bc1a6a1a0d0931209ea233a934518a9047e9253ded8f'
